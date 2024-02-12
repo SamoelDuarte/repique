b0c59a84c742e7858e4382889c1b724eb7e38b01
SamoelDuarte <129750291+SamoelDuarte@users.noreply.github.com>
2024-02-11 22:18:43 -0300

    Update AppController.php

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -14,12 +14,12 @@ class AppController extends Controller
    public function ultimosCalculos(Request $request)
    {
 
-    dd($request->all());
-       // Obter os últimos 5 cálculos resumo
-       $ultimosCalculos = CalculoResumo::orderBy('id', 'desc')->limit(5)->get();
+    // dd($request->all());
+    //    // Obter os últimos 5 cálculos resumo
+    //    $ultimosCalculos = CalculoResumo::orderBy('id', 'desc')->limit(5)->get();
 
       
        // Retornar os resultados como uma resposta JSON
-       return response()->json($ultimosCalculos);
+       return response()->json($request->all());
    }
 }
