c07d0e21d2d03b9e2034853573b04e1c5c7f466b
SamoelDuarte <129750291+SamoelDuarte@users.noreply.github.com>
2024-02-26 17:07:02 -0300

    e

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -11,15 +11,16 @@ class WebhookController extends Controller
     public function send()
     {
         $calculos = Calculo::where('send', 1)->limit(5)->get();
-
         foreach ($calculos as $key => $calculo) {
             if ($calculo->user->phone) {
                 // dd($calculo->user->phone);
                 $evento = new Events();
                 $device = Device::first();
-                dd($calculo);
-                $evento->sendMessagem($device->session, $calculo->user->phone, "Novo Cálculo da Gorjeta do dia ".$calculo.", Veja seu Valor!!!");
-                $evento->sendMessagem($device->session, $calculo->user->phone, "https://play.google.com/store/apps/details?id=br.com.repique");
+            
+              $text = ucfirst($calculo->user->name)." !!!Novo Cálculo da Gorjeta do dia ".\DateTime::createFromFormat('Y-m-d',$calculo->calculoResumo->data)->format('d/m/y').", Veja seu Valor!!!";
+              
+                $evento->sendMessagem($device->session, $calculo->user->phone,  $text );
+                // $evento->sendMessagem($device->session, $calculo->user->phone, "https://play.google.com/store/apps/details?id=br.com.repique");
             }
             $calculo->send = 0;
             $calculo->update();
